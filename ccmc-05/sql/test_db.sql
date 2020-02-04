00a0b77de547a66301e0708d2a972a1eccd5878e
haymar4 <theinyiwin@gmail.com>
2020-02-04 05:44:19 +0000

    20200204

--- ccmc-05/sql/test_db.sql
+++ ccmc-05/sql/test_db.sql
@@ -1,13 +1,8 @@
 -- データベースtest_dbを作成
-<<<<<<< HEAD
 drop databese if exists test_db;
 create database test_db;
 -- 作成したデータベースにアクセス
 
-=======
-create database test_db;
--- 作成したデータベースにアクセス
->>>>>>> 0a06f08745b7c109ef06e7c4ff8ae41ea45322c5
 use test_db;
 -- membersテーブルを作成
 create table products (
@@ -26,7 +21,6 @@ insert into products (id, name, price) values ('I-004', 'プリンタ', 9800);
 
 -- 以下のSQLはproductsテーブルのレコードについての処理として行う。
 -- exercise-S1. すべてのレコードのすべてのフィールドを抽出する
-<<<<<<< HEAD
 select * from products;
 --select id,name,price,created_at from products;
 -- exercise-S2. すべてのレコードの製品ID（idフィールド）と製品名（nameフィールド）を抽出する
@@ -62,21 +56,3 @@ select * from products;
 -- exercise-D2. すべての製品を削除する。
 delete from products;
 select * from products;
-=======
--- exercise-S2. すべてのレコードの製品ID（idフィールド）と製品名（nameフィールド）を抽出する
--- exercise-S3. 価格が40000円を超える製品のすべてのフィールドを抽出する
--- exercise-S4. 価格が3の倍数になっている製品の製品名（nameフィールド）と価格（priceフィールド）を抽出する
--- exercise-S5. 製品名が「デ」で始まるレコードの製品名（nameフィールド）を抽出する
--- exercise-S6. 製品名が「PC」で終わるレコードのすべてのフィールドを抽出する
--- exercise-S7. 製品名に「リン」が含まれているレコードのすべてのフィールドを抽出する
-
--- exercise-U1. 製品ID「I-004」の製品名を「インクジェットプリンタ」に変更する。
--- exercise-U2. 製品IDが「I-004」の製品の価格を8800円に変更する。
--- exercise-U3. すべての製品の価格を25%OFFにする。
-
--- exercise-I1. 製品IDが「I-005」、製品名「タブレットPC」、価格「68000」のレコードを追加する
-
--- exercise-D1. 製品IDが「I-005」の製品を削除する。
--- exercise-D2. すべての製品を削除する。
-
->>>>>>> 0a06f08745b7c109ef06e7c4ff8ae41ea45322c5
